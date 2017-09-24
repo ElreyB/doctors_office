2db096e88f803577e39807b053fee4a84ddef2a0
Elrey Belmonti <elbelmonti@gmail.com>
2017-09-23 20:15:37 -0700

    user can add a patient and assign doctor to them

--- app.rb
+++ app.rb
@@ -9,31 +9,51 @@ require "pry"
 
 DB = PG.connect({:dbname => 'doctor_office_test'})
 
+# DB.exec('DELETE FROM specialties *;')
+# DB.exec('DELETE FROM doctors *;')
+# DB.exec('DELETE FROM patients *;')
+
+specialties = []
+File.open('specialties.txt', 'r') do |file|
+  while file_line = file.gets
+    specialties.push(file_line.delete("\n"))
+  end
+end
+specialties = specialties.sort
+specialties.each do |specialty|
+  new_specialty = Specialty.new({name: specialty})
+  new_specialty.save
+end
+
 get('/') do
   erb(:index)
 end
 
 get('/doctors') do
-  specialties = []
-  File.open('specialties.txt', 'r') do |file|
-    while file_line = file.gets
-      specialties.push(file_line.delete("\n"))
-    end
-  end
-  specialties = specialties.sort
-  specialties.each do |specialty|
-    new_specialty = Specialty.new({name: specialty})
-    new_specialty.save
-  end
   @specialties = Specialty.all
   @doctors = Doctor.all
-  erb(:doctors)
+  erb(:doctors_list)
+end
+
+get('/patients') do
+  @doctors = Doctor.all
+  @patients = Patient.all
+  erb(:patients_list)
 end
 
 post("/doctor") do
-  specialty_id = params['specialty'].to_i
+  specialty_id = params['specialty-id'].to_i
   doctor_name = params['doctor-name']
   doctor = Doctor.new({name: doctor_name, specialty_id: specialty_id})
   doctor.save
   redirect 'doctors'
+end
+
+post('/patient') do
+  doctor_id = params['doctor-id'].to_i
+  patient_name = params['patient-name']
+  patient_birthday = params['patient-birthday']
+  patient = Patient.new({name: patient_name, birthday: patient_birthday, doctor_id: doctor_id})
+  patient.save
+  redirect 'patients'
 end

ba1371bc4d6a81918f63f8deb55f5413157b34e1
Elrey Belmonti <elbelmonti@gmail.com>
2017-09-23 19:31:59 -0700

    allow user to add a doctor with specialty to list of doctors

--- app.rb
+++ app.rb
@@ -26,5 +26,14 @@ get('/doctors') do
     new_specialty.save
   end
   @specialties = Specialty.all
+  @doctors = Doctor.all
   erb(:doctors)
+end
+
+post("/doctor") do
+  specialty_id = params['specialty'].to_i
+  doctor_name = params['doctor-name']
+  doctor = Doctor.new({name: doctor_name, specialty_id: specialty_id})
+  doctor.save
+  redirect 'doctors'
 end
